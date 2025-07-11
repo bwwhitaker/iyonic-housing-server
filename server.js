9d7f264518ca5c693bec5194dd4a7e63c142407f
Brian Whitaker <43477669+bwwhitaker@users.noreply.github.com>
2025-07-11 14:32:55 -0700

    Enable Calculator Functionality

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Enable CORS for all routes
+const aduCalculator = require('./Routes/ADUCalculator');
+
+// Enable CORS and JSON body parsing
 app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(express.json()); // <- Needed for parsing POST JSON bodies
 
 // Landing Page
 app.get('/', (req, res) => {
@@ -23,7 +26,6 @@ app.get('/api/states', (req, res) => {
 	res.json({ states: sortedStates });
 });
 
-// Route to get cities by state name
 app.get('/api/cities', (req, res) => {
 	const state = req.query.state;
 	if (!state) {
@@ -34,10 +36,19 @@ app.get('/api/cities', (req, res) => {
 	if (!cities) {
 		return res.status(404).json({ error: `No cities found for state: ${state}` });
 	}
+	const sortedCities = cities.sort();
 
-	res.json({ cities });
+	res.json({ cities: sortedCities });
 });
 
+app.get('/api/statePrices', (req, res) => {});
+
+// Mount calculator API
+app.use('/api/aduCalculator', aduCalculator);
+app.use('/api/aduCalculator/prices', aduCalculator);
+app.use('/api/aduCalculator/pricesByState', aduCalculator);
+
+// Start the server
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
